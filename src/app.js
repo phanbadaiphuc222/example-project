741e07a970a2d4ccc68e81b08457ca7b02c16a6b
Phan Ba Dai Phuc <phucb1910688@student.ctu.edu.vn>
2022-09-16 09:48:53 +0700

    Implement an error handling middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const contactController = require('./controllers/contact.controller');
+const ApiError = require('./api-error');
+const { application } = require('express');
 
 app.use(cors());
 app.use(express.json());
@@ -22,4 +24,18 @@ app.route('/api/contatcs/:id')
     .put(contactController.update)
     .delete(contactController.delete);
 
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+    
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
+});
 module.exports = app;
