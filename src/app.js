8c3369add8089eeb4115fc203a7b9a2d55219cec
Phan Ba Dai Phuc <phucb1910688@student.ctu.edu.vn>
2022-09-16 09:27:28 +0700

    Define routes for managing contacts

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-
 const app = express();
+const contactController = require('./controllers/contact.controller');
 
 app.use(cors());
 app.use(express.json());
@@ -9,5 +9,17 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json ({ message: 'Welcome to contact book application.'});
 });
+
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
+
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+
+app.route('/api/contatcs/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
 
 module.exports = app;
---
+++ src/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+    return res.send({ message: 'create handler'});
+};
+
+exports.findAll = (req, res) => {
+    return res.send({ message: 'findAll handler'});
+};
+
+exports.findOne = (req, res) => {
+    return res.send({ message: 'findOne handler'});
+};
+
+exports.update = (req, res) => {
+    return res.send({ message: 'findOne handler'});
+};
+
+exports.delete = (req, res) => {
+    return res.send({ message: 'findOne handler'});
+};
+
+exports.deleteAll = (req, res) => {
+    return res.send({ messsage: 'deleteAll handler'});
+};
+
+exports.findAllFavorite = (req, res) => {
+    return res.send({ message: 'findAllFavorite handler'});
+};
