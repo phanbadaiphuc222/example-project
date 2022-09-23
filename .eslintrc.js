37c301933f1551c6f4b47d054277d3478d11f88a
phanbadaiphuc222 <phanbadaiphuc123@gmail.com>
2022-09-23 08:53:31 +0700

    Configure ESLint for the project

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    env: {
+         node: true,
+         commonjs: true,
+         es2822: true
+    },
+    extends: 'eslint:recommended',
+    rules: {
+         "no-unused-vars": "off"
+    }
+}
